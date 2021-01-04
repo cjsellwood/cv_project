f59c50e943e2e3bb61e7006b121c3c4103c6ef38
cjsellwood <c.sellwood22@gmail.com>
2021-01-04 15:47:48 +1100

    Finished styling

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -4,56 +4,67 @@ const Education = (props) => {
   // form - Institution Name, Course Name, Start Date, End Date
   const formsArray = [];
   const displayArray = [];
-  
+
   for (let i = 0; i < props.education.length; i++) {
     formsArray.push(
-      <form key={i}>
-        <label>Course Name</label>
-        <input
-          type="text"
-          key="course"
-          data-id="course"
-          data-index={i}
-          value={props.education[i].course}
-          onChange={props.editEducation}
-        />
-        <label>Institution Name</label>
-        <input
-          type="text"
-          key="name"
-          data-id="name"
-          data-index={i}
-          value={props.education[i].name}
-          onChange={props.editEducation}
-        />
-        <label>Start Year</label>
-        <input
-          type="text"
-          key="start"
-          data-id="start"
-          data-index={i}
-          value={props.education[i].start}
-          onChange={props.editEducation}
-        />
-        <label>Finish Year</label>
-        <input
-          type="text"
-          key="end"
-          data-id="end"
-          data-index={i}
-          value={props.education[i].end}
-          onChange={props.editEducation}
-        />
-        <label>Details</label>
-        <textarea
-          type="text"
-          key="details"
-          data-id="details"
-          data-index={i}
-          value={props.education[i].details}
-          onChange={props.editEducation}
-        />
+      <form className="educationForm" key={i}>
+        <div>
+          <label>Course Name</label>
+          <input
+            type="text"
+            key="course"
+            data-id="course"
+            data-index={i}
+            value={props.education[i].course}
+            onChange={props.editEducation}
+          />
+        </div>
+        <div>
+          <label>Institution Name</label>
+          <input
+            type="text"
+            key="name"
+            data-id="name"
+            data-index={i}
+            value={props.education[i].name}
+            onChange={props.editEducation}
+          />
+        </div>
+        <div>
+          <label>Start Year</label>
+          <input
+            type="text"
+            key="start"
+            data-id="start"
+            data-index={i}
+            value={props.education[i].start}
+            onChange={props.editEducation}
+          />
+        </div>
+        <div>
+          <label>Finish Year</label>
+          <input
+            type="text"
+            key="end"
+            data-id="end"
+            data-index={i}
+            value={props.education[i].end}
+            onChange={props.editEducation}
+          />
+        </div>
+        <div className="details">
+          <label>Details</label>
+          <textarea
+            type="text"
+            key="details"
+            data-id="details"
+            data-index={i}
+            value={props.education[i].details}
+            onChange={props.editEducation}
+          />
+        </div>
         <button
+          className="deleteButton"
           type="button"
           data-index={i}
           onClick={props.deleteEducationForm}
@@ -63,13 +74,16 @@ const Education = (props) => {
       </form>
     );
     displayArray.push(
-      <div key={i}>
-        <h3>{props.education[i].course}</h3>
+      <div className="displayDiv" key={i}>
+        <div>
+          <h3>{props.education[i].course}</h3>
+          <span>
+            {props.education[i].start}{" "}
+            {props.education[i].start !== "" ? "-" : ""}{" "}
+            {props.education[i].end}
+          </span>
+        </div>
         <h4>{props.education[i].name}</h4>
-        <span>
-          {props.education[i].start}{" "}
-          {props.education[i].start !== "" ? "-" : ""} {props.education[i].end}
-        </span>
         <p>{props.education[i].details}</p>
       </div>
     );
@@ -77,10 +91,14 @@ const Education = (props) => {
 
   return (
     <section className="education">
-      <h2>Education</h2>
+      <h2 className="section-title">Education</h2>
       {props.editing ? formsArray : displayArray}
       {props.editing ? (
-        <button onClick={props.addEducationForm} type="button">
+        <button
+          className="addButton"
+          onClick={props.addEducationForm}
+          type="button"
+        >
           Add
         </button>
       ) : null}
--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -6,65 +6,84 @@ const Work = (props) => {
   const displayArray = [];
   for (let i = 0; i < props.work.length; i++) {
     formsArray.push(
-      <form key={i}>
-        <label>Role</label>
-        <input
-          type="text"
-          key="role"
-          data-id="role"
+      <form className="workForm" key={i}>
+        <div>
+          <label>Role</label>
+          <input
+            type="text"
+            key="role"
+            data-id="role"
+            data-index={i}
+            value={props.work[i].role}
+            onChange={props.editWork}
+          />
+        </div>
+
+        <div>
+          <label>Company</label>
+          <input
+            type="text"
+            key="company"
+            data-id="company"
+            data-index={i}
+            value={props.work[i].company}
+            onChange={props.editWork}
+          />
+        </div>
+        <div>
+          <label>Start Year</label>
+          <input
+            type="text"
+            key="start"
+            data-id="start"
+            data-index={i}
+            value={props.work[i].start}
+            onChange={props.editWork}
+          />
+        </div>
+        <div>
+          <label>Finish Year</label>
+          <input
+            type="text"
+            key="end"
+            data-id="end"
+            data-index={i}
+            value={props.work[i].end}
+            onChange={props.editWork}
+          />
+        </div>
+        <div className="details">
+          <label>Details</label>
+          <textarea
+            type="text"
+            key="details"
+            data-id="details"
+            data-index={i}
+            value={props.work[i].details}
+            onChange={props.editWork}
+          />
+        </div>
+        <button
+          className="deleteButton"
+          type="button"
           data-index={i}
-          value={props.work[i].role}
-          onChange={props.editWork}
-        />
-        <label>Company</label>
-        <input
-          type="text"
-          key="company"
-          data-id="company"
-          data-index={i}
-          value={props.work[i].company}
-          onChange={props.editWork}
-        />
-        <label>Start Year</label>
-        <input
-          type="text"
-          key="start"
-          data-id="start"
-          data-index={i}
-          value={props.work[i].start}
-          onChange={props.editWork}
-        />
-        <label>Finish Year</label>
-        <input
-          type="text"
-          key="end"
-          data-id="end"
-          data-index={i}
-          value={props.work[i].end}
-          onChange={props.editWork}
-        />
-        <label>Details</label>
-        <textarea
-          type="text"
-          key="details"
-          data-id="details"
-          data-index={i}
-          value={props.work[i].details}
-          onChange={props.editWork}
-        />
-        <button type="button" data-index={i} onClick={props.deleteWorkForm}>
+          onClick={props.deleteWorkForm}
+        >
           Delete
         </button>
       </form>
     );
     displayArray.push(
-      <div key={i}>
-        <h3>{props.work[i].role}</h3>
+      <div className="displayDiv" key={i}>
+        <div>
+          <h3>{props.work[i].role}</h3>
+          <span>
+            {props.work[i].start} {props.work[i].start !== "" ? "-" : ""}{" "}
+            {props.work[i].end}
+          </span>
+        </div>
         <h4>{props.work[i].company}</h4>
-        <span>
-          {props.work[i].start} {props.work[i].start !== "" ? "-" : ""}{" "}
-          {props.work[i].end}
-        </span>
+
         <p>{props.work[i].details}</p>
       </div>
     );
@@ -72,10 +91,10 @@ const Work = (props) => {
 
   return (
     <section className="work">
-      <h2>Work Experience</h2>
+      <h2 className="section-title">Work Experience</h2>
       {props.editing ? formsArray : displayArray}
       {props.editing ? (
-        <button onClick={props.addWorkForm} type="button">
+        <button className="addButton" onClick={props.addWorkForm} type="button">
           Add
         </button>
       ) : null}
